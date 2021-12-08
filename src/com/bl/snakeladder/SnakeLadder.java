1145be0232d38cc01a00b2505d874af5be911c81
akshaydig <you@example.com>
2021-12-08 15:25:39 +0530

    Added: code to start with single player at start position

---
+++ src/com/bl/snakeladder/SnakeLadder.java
@@ -0,0 +1,9 @@
+package com.bl.snakeladder;
+
+public class SnakeLadder {
+	public static void main(String[] args) {
+		int startPosition = 0;
+
+		System.out.println("Player start Game with Position : " + startPosition);
+	}
+}
